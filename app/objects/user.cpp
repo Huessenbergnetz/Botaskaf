34bd12a0ff58b27459d7ba4fdde9a30a132dc8c8
Buschmann <mf@huessenbergnetz.de>
2024-04-16 17:17:10 +0200

    Add User object

---
+++ app/objects/user.cpp
@@ -0,0 +1,444 @@
+﻿/*
+ * SPDX-FileCopyrightText: (C) 2024 Matthias Fehring <https://www.huessenbergnetz.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "user.h"
+
+#include <QDebug>
+#include <QMetaEnum>
+#include <QMetaObject>
+
+using namespace Qt::Literals::StringLiterals;
+
+class UserData : public QSharedData // NOLINT(cppcoreguidelines-special-member-functions)
+{
+public:
+    UserData() noexcept = default;
+    UserData(User::dbid_t _id,
+             User::Type _type,
+             const QString &_email,
+             const QString &_displayName,
+             const QDateTime &_created,
+             const QDateTime &_updated,
+             const QDateTime &_lastSeen,
+             const QDateTime &_lockedAt,
+             User::dbid_t _lockedById,
+             const QString &_lockedByName,
+             const QVariantMap &_settings)
+        : QSharedData()
+        , email{_email}
+        , displayName{_displayName}
+        , lockedByName{_lockedByName}
+        , created{_created}
+        , updated{_updated}
+        , lastSeen{_lastSeen}
+        , lockedAt{_lockedAt}
+        , settings{_settings}
+        , id{_id}
+        , lockedById{_lockedById}
+        , type{_type}
+    {
+        created.setTimeSpec(Qt::UTC);
+        updated.setTimeSpec(Qt::UTC);
+        if (lastSeen.isValid()) {
+            lastSeen.setTimeSpec(Qt::UTC);
+        }
+        if (lockedAt.isValid()) {
+            lockedAt.setTimeSpec(Qt::UTC);
+        }
+    }
+
+    UserData(const UserData &) noexcept   = default;
+    UserData &operator=(const UserData &) = delete;
+    ~UserData() noexcept                  = default;
+
+    QString email;
+    QString displayName;
+    QString lockedByName;
+    QDateTime created;
+    QDateTime updated;
+    QDateTime lastSeen;
+    QDateTime lockedAt;
+    QVariantMap settings;
+    User::dbid_t id{0};
+    User::dbid_t lockedById{0};
+    User::Type type{User::Invalid};
+};
+
+User::User() noexcept = default;
+
+User::User(dbid_t id,
+           Type type,
+           const QString &email,
+           const QString &displayName,
+           const QDateTime &created,
+           const QDateTime &updated,
+           const QDateTime &lastSeen,
+           const QDateTime &lockedAt,
+           dbid_t lockedById,
+           const QString &lockedByName,
+           const QVariantMap &settings)
+    : data{new UserData{id,
+                        type,
+                        email,
+                        displayName,
+                        created,
+                        updated,
+                        lastSeen,
+                        lockedAt,
+                        lockedById,
+                        lockedByName,
+                        settings}}
+{
+}
+
+User::User(const User &user) noexcept = default;
+
+User::User(User &&user) noexcept = default;
+
+User &User::operator=(const User &user) noexcept = default;
+
+User &User::operator=(User &&user) noexcept = default;
+
+User::~User() noexcept = default;
+
+User::dbid_t User::id() const noexcept
+{
+    return data ? data->id : 0;
+}
+
+User::Type User::type() const noexcept
+{
+    return data ? data->type : Invalid;
+}
+
+QString User::email() const noexcept
+{
+    return data ? data->email : QString();
+}
+
+QString User::displayName() const noexcept
+{
+    return data ? data->displayName : QString();
+}
+
+QDateTime User::created() const noexcept
+{
+    return data ? data->created : QDateTime();
+}
+
+QDateTime User::updated() const noexcept
+{
+    return data ? data->updated : QDateTime();
+}
+
+QDateTime User::lastSeen() const noexcept
+{
+    return data ? data->lastSeen : QDateTime();
+}
+
+QDateTime User::lockedAt() const noexcept
+{
+    return data ? data->lockedAt : QDateTime();
+}
+
+User::dbid_t User::lockedById() const noexcept
+{
+    return data ? data->lockedById : 0;
+}
+
+QString User::lockedByName() const noexcept
+{
+    return data ? data->lockedByName : QString();
+}
+
+QVariantMap User::settings() const
+{
+    return data ? data->settings : QVariantMap();
+}
+
+QVariant User::setting(const QString &key, const QVariant &defValue) const
+{
+    return data ? data->settings.value(key, defValue) : QVariant();
+}
+
+bool User::isAdmin() const noexcept
+{
+    return data && data->type >= Administrator;
+}
+
+bool User::isValid() const noexcept
+{
+    return data && data->id > 0 && data->type != Invalid;
+}
+
+QJsonObject User::toJson() const
+{
+    if (isNull() || !isValid()) {
+        return {};
+    }
+
+    return {{u"id"_s, static_cast<qint64>(data->id)},
+            {u"type"_s, static_cast<int>(data->type)},
+            {u"email"_s, data->email},
+            {u"displayName"_s, data->displayName},
+            {u"created"_s, data->created.toString(Qt::ISODateWithMs)},
+            {u"updated"_s, data->updated.toString(Qt::ISODateWithMs)},
+            {u"lastSeen"_s,
+             data->lastSeen.isNull() ? QJsonValue() : data->lastSeen.toString(Qt::ISODateWithMs)},
+            {u"lockedAt"_s,
+             data->lockedAt.isNull() ? QJsonValue() : data->lockedAt.toString(Qt::ISODateWithMs)},
+            {u"lockedById"_s, static_cast<qint64>(data->lockedById)},
+            {u"lockedByName"_s, data->lockedByName.isEmpty() ? QJsonValue() : data->lockedByName},
+            {u"settings"_s, QJsonObject::fromVariantMap(data->settings)},
+            {u"isAdmin"_s, isAdmin()}};
+}
+
+bool User::operator==(const User &other) const noexcept
+{
+    if (data == other.data) {
+        return true;
+    }
+
+    if (id() != other.id()) {
+        return false;
+    }
+
+    if (type() != other.type()) {
+        return false;
+    }
+
+    if (email() != other.email()) {
+        return false;
+    }
+
+    if (displayName() != other.displayName()) {
+        return false;
+    }
+
+    if (created() != other.created()) {
+        return false;
+    }
+
+    if (updated() != other.updated()) {
+        return false;
+    }
+
+    if (lastSeen() != other.lastSeen()) {
+        return false;
+    }
+
+    if (lockedAt() != other.lockedAt()) {
+        return false;
+    }
+
+    if (lockedById() != other.lockedById()) {
+        return false;
+    }
+
+    if (lockedByName() != other.lockedByName()) {
+        return false;
+    }
+
+    if (settings() != other.settings()) {
+        return false;
+    }
+
+    return true;
+}
+
+User::Type User::typeStringToEnum(const QString &str)
+{
+    if (str.compare("disabled"_L1, Qt::CaseInsensitive) == 0) {
+        return User::Disabled;
+    } else if (str.compare("registered"_L1, Qt::CaseInsensitive) == 0) {
+        return User::Registered;
+    } else if (str.compare("manager"_L1, Qt::CaseInsensitive) == 0) {
+        return User::Manager;
+    } else if (str.compare("administrator"_L1, Qt::CaseInsensitive) == 0) {
+        return User::Administrator;
+    } else if (str.compare("superuser"_L1, Qt::CaseInsensitive) == 0) {
+        return User::SuperUser;
+    } else {
+        return User::Invalid;
+    }
+}
+
+QString User::typeEnumToString(User::Type type)
+{
+    if (type != User::Invalid) {
+        const auto mo = User::staticMetaObject;
+        const auto me = mo.enumerator(mo.indexOfEnumerator("Type"));
+
+        return QString::fromLocal8Bit(me.valueToKey(static_cast<int>(type)));
+    }
+
+    return {};
+}
+
+QStringList User::supportedTypes()
+{
+    QStringList lst;
+
+    const auto mo = User::staticMetaObject;
+    const auto me = mo.enumerator(mo.indexOfEnumerator("Type"));
+
+    lst.reserve(me.keyCount() - 1);
+
+    for (int i = 1; i < me.keyCount(); ++i) {
+        lst.append(QString::fromLocal8Bit(me.key(i)));
+    }
+
+    return lst;
+}
+
+QStringList User::typeValues()
+{
+    QStringList lst;
+
+    const auto mo = User::staticMetaObject;
+    const auto me = mo.enumerator(mo.indexOfEnumerator("Type"));
+
+    lst.reserve(me.keyCount() - 1);
+
+    for (int i = 1; i < me.keyCount(); ++i) {
+        lst << QString::number(me.value(i));
+    }
+
+    return lst;
+}
+
+QStringList User::typeValues(User::Type below)
+{
+    QStringList lst;
+    const int belowInt = static_cast<int>(below);
+
+    const auto mo = User::staticMetaObject;
+    const auto me = mo.enumerator(mo.indexOfEnumerator("Type"));
+
+    for (int i = 1; i < me.keyCount(); ++i) {
+        const int val = me.value(i);
+        if (val < belowInt) {
+            lst << QString::number(val); // clazy:exclude=reserve-candidates
+        }
+    }
+
+    return lst;
+}
+
+User::dbid_t User::toDbId(qulonglong id, bool *ok)
+{
+    if (id > static_cast<qulonglong>(std::numeric_limits<User::dbid_t>::max())) {
+        if (ok) {
+            *ok = false;
+        }
+        return 0;
+    }
+
+    if (ok) {
+        *ok = true;
+    }
+
+    return static_cast<User::dbid_t>(id);
+}
+
+User::dbid_t User::toDbId(const QVariant &var, bool *ok)
+{
+    bool _ok      = false;
+    const auto id = var.toULongLong(&_ok);
+
+    if (_ok) {
+        return toDbId(id, ok);
+    }
+
+    if (ok) {
+        *ok = false;
+    }
+
+    return 0;
+}
+
+User::dbid_t User::toDbId(const QString &str, bool *ok)
+{
+    bool _ok      = false;
+    const auto id = str.toULongLong(&_ok);
+
+    if (_ok) {
+        return toDbId(id, ok);
+    }
+
+    if (ok) {
+        *ok = false;
+    }
+
+    return 0;
+}
+
+QDebug operator<<(QDebug dbg, const User &user)
+{
+    QDebugStateSaver saver(dbg);
+    dbg.nospace() << "User(";
+    if (!user.isNull()) {
+        if (user.isValid()) {
+            dbg << "DI: " << user.id();
+            dbg << ", Email: " << user.email();
+            dbg << ", Type: " << user.type();
+            dbg << ", Display Name: " << user.displayName();
+        } else {
+            dbg << "INVALID";
+        }
+    }
+    dbg << ')';
+    return dbg;
+}
+
+QDataStream &operator<<(QDataStream &out, const User &user)
+{
+    if (!user.isNull()) {
+        out << user.data->id << static_cast<qint32>(user.data->type) << user.data->email
+            << user.data->displayName << user.data->created << user.data->updated
+            << user.data->lastSeen << user.data->lockedAt << user.data->lockedById
+            << user.data->lockedByName << user.data->settings;
+    } else {
+        out << static_cast<User::dbid_t>(0);
+    }
+    return out;
+}
+
+QDataStream &operator>>(QDataStream &in, User &user)
+{
+    User::dbid_t id = 0;
+    in >> id;
+
+    if (id == 0) {
+        user.clear();
+    } else {
+        if (user.data == nullptr) {
+            user.data = new UserData; // NOLINT(cppcoreguidelines-owning-memory)
+        }
+
+        user.data->id  = id;
+        qint32 typeInt = -1;
+        in >> typeInt;
+        user.data->type = static_cast<User::Type>(typeInt);
+        in >> user.data->email;
+        in >> user.data->displayName;
+        in >> user.data->created;
+        in >> user.data->updated;
+        in >> user.data->lastSeen;
+        in >> user.data->lockedAt;
+        in >> user.data->lockedById;
+        in >> user.data->lockedByName;
+        in >> user.data->settings;
+    }
+
+    return in;
+}
+
+void swap(User &a, User &b) noexcept
+{
+    a.swap(b);
+}
+
+#include "moc_user.cpp"
---
+++ app/objects/user.h
@@ -0,0 +1,376 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2024 Matthias Fehring <https://www.huessenbergnetz.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef HBNBOTA_USER_H
+#define HBNBOTA_USER_H
+
+#include <QDateTime>
+#include <QJsonObject>
+#include <QObject>
+#include <QSharedDataPointer>
+
+class UserData;
+
+/*!
+ * \brief Contains data about users and provides user management methods.
+ */
+class User
+{
+    Q_GADGET
+    /*!
+     * \brief The database ID.
+     */
+    Q_PROPERTY(User::dbid_t id READ id CONSTANT)
+    /*!
+     * \brief The user type.
+     */
+    Q_PROPERTY(User::Type type READ type CONSTANT)
+    /*!
+     * \brief The user’s email address.
+     */
+    Q_PROPERTY(QString email READ email CONSTANT)
+    /*!
+     * \brief The user’s display name.
+     */
+    Q_PROPERTY(QString displayName READ displayName CONSTANT)
+    /*!
+     * \brief UTC date and time the user has been created.
+     */
+    Q_PROPERTY(QDateTime created READ created CONSTANT)
+    /*!
+     * \brief UTC date and time the user has been last updated.
+     */
+    Q_PROPERTY(QDateTime updated READ updated CONSTANT)
+    /*!
+     * \brief UTC date and time the user has been last seen.
+     *
+     * If the user has never been seen, this will be a null datetime.
+     */
+    Q_PROPERTY(QDateTime lastSeen READ lastSeen CONSTANT)
+    /*!
+     * \brief UTC date and time the user has been locked for editing.
+     *
+     * If the user has not been locked, this will be a null datetime.
+     */
+    Q_PROPERTY(QDateTime lockedAt READ lockedAt CONSTANT)
+    /*!
+     * \brief Database ID of the user that locked this user for editing.
+     *
+     * If the user has not been locked, this will be \c 0.
+     */
+    Q_PROPERTY(User::dbid_t lockedById READ lockedById CONSTANT)
+    /*!
+     * \brief Display name of the user that locked this user for editing.
+     *
+     * If the user has not been locked, this will be a null string.
+     */
+    Q_PROPERTY(QString lockedByName READ lockedByName CONSTANT)
+    /*!
+     * \brief Map of settings for this user.
+     */
+    Q_PROPERTY(QVariantMap settings READ settings CONSTANT)
+    /*!
+     * \brief \c true if this user is an administrator, otherwise \c false.
+     */
+    Q_PROPERTY(bool isAdmin READ isAdmin CONSTANT)
+public:
+    /*!
+     * \brief Database ID type.
+     */
+    using dbid_t = quint32;
+
+    /*!
+     * \brief The user type.
+     */
+    enum Type : int {
+        Invalid       = -1,
+        Disabled      = 0,
+        Registered    = 16,
+        Manager       = 32,
+        Administrator = 64,
+        SuperUser     = 127
+    };
+    Q_ENUM(Type)
+
+    /*!
+     * \brief Constructs a new \a null %User object.
+     *
+     * isNull() returns \c true for this, isValid() returns \c false.
+     */
+    User() noexcept;
+    /*!
+     * \brief Constructs a new %User object with the given parameters.
+     * \param id            The database id.
+     * \param type          The user type.
+     * \param email         The email address.
+     * \param displayName   The display name.
+     * \param created       UTC date and time the user has been created.
+     * \param updated       UTC date and time the user has been updated.
+     * \param lastSeen      UTC date and time the user has been last seen.
+     * \param lockedAt      UTC date and time the user has been locked for editing.
+     * \param lockedById    Database id of the the user that locked this user for editing.
+     * \param lockedByName  Display name of the user that locked this user for editing.
+     * \param settings      Settings for this user.
+     */
+    User(dbid_t id,
+         Type type,
+         const QString &email,
+         const QString &displayName,
+         const QDateTime &created,
+         const QDateTime &updated,
+         const QDateTime &lastSeen,
+         const QDateTime &lockedAt,
+         dbid_t lockedById,
+         const QString &lockedByName,
+         const QVariantMap &settings);
+    /*!
+     * \brief Constructs a copy of \a other.
+     */
+    User(const User &other) noexcept;
+    /*!
+     * \brief Move-constructs a %User instance , making it point at the same object that \a other
+     * was pointing to.
+     */
+    User(User &&other) noexcept;
+    /*!
+     * \brief Assigns \a other to this user and returns a reference to this user.
+     */
+    User &operator=(const User &other) noexcept;
+    /*!
+     * \brief Move-assigns \a other to this %User instance.
+     */
+    User &operator=(User &&other) noexcept;
+    /*!
+     * \brief Destroys the %User object.
+     */
+    ~User() noexcept;
+
+    /*!
+     * \brief Swaps user \a other with this user.
+     *
+     * This operation is very fast and never fails.
+     */
+    void swap(User &other) noexcept { data.swap(other.data); }
+
+    /*!
+     * \brief Returns the user’s database ID.
+     */
+    [[nodiscard]] dbid_t id() const noexcept;
+
+    /*!
+     * \brief Returns the type of this user.
+     */
+    [[nodiscard]] Type type() const noexcept;
+
+    /*!
+     * \brief Returns the user’s email address.
+     */
+    [[nodiscard]] QString email() const noexcept;
+
+    /*!
+     * \brief Returns the user’s display name.
+     */
+    [[nodiscard]] QString displayName() const noexcept;
+
+    /*!
+     * \brief Returns the UTC date and time the user has been created.
+     */
+    [[nodiscard]] QDateTime created() const noexcept;
+
+    /*!
+     * \brief Returns the UTC date and time the user has been updated.
+     */
+    [[nodiscard]] QDateTime updated() const noexcept;
+
+    /*!
+     * \brief Returns the UTC date and time the user has been last seen.
+     *
+     * If the user never logged in, this will return a \c null datetime.
+     */
+    [[nodiscard]] QDateTime lastSeen() const noexcept;
+
+    /*!
+     * \brief Returns the UTC date and time the user data has been locked.
+     *
+     * If the user has not been locked, this will return a \c null datetime.
+     */
+    [[nodiscard]] QDateTime lockedAt() const noexcept;
+
+    /*!
+     * \brief Returns the database id of the user that locked this user.
+     *
+     * If the user has not been locked, this will return \c 0.
+     */
+    [[nodiscard]] dbid_t lockedById() const noexcept;
+
+    /*!
+     * \brief Returns the display name of the user that locked this user.
+     *
+     * If the user has not been locked, this will return an empty string.
+     */
+    [[nodiscard]] QString lockedByName() const noexcept;
+
+    /*!
+     * \brief Returns the user’s settings.
+     */
+    [[nodiscard]] QVariantMap settings() const;
+
+    /*!
+     * \brief Returns a specific settings value for this user identified by \a key.
+     *
+     * If \a key is not present in the settings and \a defValue is given, this will
+     * be returned instead.
+     */
+    [[nodiscard]] QVariant setting(const QString &key, const QVariant &defValue = QVariant()) const;
+
+    /*!
+     * \brief Returns \c true if this user is of type Administrator or higher.
+     */
+    [[nodiscard]] bool isAdmin() const noexcept;
+
+    /*!
+     * \brief Returns \c true if this user is valid.
+     *
+     * A user is valid if the id() is greater than \c 0 and if the type()
+     * is not Invalid.
+     */
+    [[nodiscard]] bool isValid() const noexcept;
+
+    /*!
+     * \brief Returns \c true if this is a \c null user object.
+     */
+    [[nodiscard]] bool isNull() const noexcept { return !data; }
+
+    /*!
+     * \brief Clears the user object.
+     *
+     * After calling this, isNull() will return \c true and isValid() will
+     * return \c false.
+     */
+    void clear()
+    {
+        if (!isNull())
+            *this = User();
+    }
+
+    /*!
+     * \brief Converts the user object into a JSON object.
+     *
+     * Property names will be the same as for the %User object.
+     */
+    [[nodiscard]] QJsonObject toJson() const;
+
+    /*!
+     * \brief Returns \c true if \a other user is equal to this user; otherwise \c false.
+     *
+     * This compares all properties.
+     */
+    bool operator==(const User &other) const noexcept;
+
+    /*!
+     * \brief Returns \c true if \a other user is not equal to this user; otherwise \c false.
+     *
+     * This compares all properties.
+     */
+    bool operator!=(const User &other) const noexcept { return !(*this == other); }
+
+    /*!
+     * \brief Converts \a str to enum Type.
+     */
+    static Type typeStringToEnum(const QString &str);
+
+    /*!
+     * \brief Converts enum \y type to a string.
+     */
+    static QString typeEnumToString(Type type);
+
+    /*!
+     * \brief Returns a string list of supported user type names.
+     *
+     * This will contain the key names.
+     */
+    static QStringList supportedTypes();
+
+    /*!
+     * \brief Returns a string list of supported user type values.
+     *
+     * This will contain the key values.
+     */
+    static QStringList typeValues();
+
+    /*!
+     * \brief Returns  a string list of supported user type values \a below a specific Type.
+     *
+     * This will contain the key values.
+     */
+    static QStringList typeValues(Type below);
+
+    /*!
+     * \brief Returns \a id casted into dbid_t.
+     *
+     * Returns \c 0 if the value would overflow the dbid_t.
+     *
+     * If \a ok is not \c nullptr, failure is reported by setting \a *ok to \c false,
+     * and success by setting \a *ok to \c true.
+     */
+    static dbid_t toDbId(qulonglong id, bool *ok = nullptr);
+
+    /*!
+     * \brief Returns \a var converted to dbid_t.
+     *
+     * Returns \c 0 if the conversion fails.
+     *
+     * If \a ok is not \c nullptr, failure is reported by setting \a *ok to \c false,
+     * and success by setting \a *ok to \c true.
+     */
+    static dbid_t toDbId(const QVariant &var, bool *ok = nullptr);
+
+    /*!
+     * \brief Returns \a str converted to dbid_t.
+     *
+     * Returns \c 0 if the conversion fails.
+     *
+     * If \a ok is not \c nullptr, failure is reported by setting \a *ok to \c false,
+     * and success by setting \a *ok to \c true.
+     */
+    static dbid_t toDbId(const QString &str, bool *ok = nullptr);
+
+private:
+    QSharedDataPointer<UserData> data;
+
+    friend QDataStream &operator<<(QDataStream &out, const User &user);
+    friend QDataStream &operator>>(QDataStream &in, User &user);
+};
+
+Q_DECLARE_METATYPE(User)
+Q_DECLARE_TYPEINFO(User, Q_RELOCATABLE_TYPE); // NOLINT(modernize-type-traits)
+
+/*!
+ * \related
+ * \brief Writes the \a user to the debug stream \a dbg and returns a reference to the stream.
+ */
+QDebug operator<<(QDebug dbg, const User &user);
+
+/*!
+ * \related User
+ * \brief Writes \a user to stream \a out. Returns a reference to the stream.
+ */
+QDataStream &operator<<(QDataStream &out, const User &user);
+
+/*!
+ * \related User
+ * \brief Reds a user from stream \a in into \a user. Returns a reference to the stream.
+ */
+QDataStream &operator>>(QDataStream &in, User &user);
+
+/*!
+ * \related User
+ * \brief Swaps user \a a with user \a b.
+ *
+ * This operation is very fast and never fails.
+ */
+void swap(User &a, User &b) noexcept;
+
+#endif // HBNBOTA_USER_H
