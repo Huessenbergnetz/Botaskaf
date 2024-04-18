dbaab49568fc507af2f423af307d7c6a90a3175c
Buschmann <mf@huessenbergnetz.de>
2024-04-18 10:04:57 +0200

    Add more tests for User object

--- tests/testuser.cpp
+++ tests/testuser.cpp
@@ -5,6 +5,8 @@
 
 #include "objects/user.h"
 
+#include <limits>
+
 #include <QDataStream>
 #include <QTest>
 
@@ -27,6 +29,13 @@ private slots:
     void testMove();
     void testSwap();
     void testClear();
+    void testComparison();
+    void testDatastream();
+    void testToJson();
+    void testTypeStringToEnum();
+    void testTypeEnumToString();
+    void testSupportedTypes();
+    void testToDbId();
 };
 
 // NOLINTBEGIN(cppcoreguidelines-avoid-do-while)
@@ -207,6 +216,264 @@ void UserTest::testClear()
         1, User::Registered, u"user@example.net"_s, u"John Doe"_s, now, now, now, {}, 0, {}, {});
     u1.clear();
     QVERIFY(u1.isNull());
+}
+
+void UserTest::testComparison()
+{
+    const QDateTime now = QDateTime::currentDateTimeUtc();
+
+    User u1(
+        1, User::Registered, u"user@example.net"_s, u"John Doe"_s, now, now, now, {}, 0, {}, {});
+    User u2(2, User::Manager, u"user@example.com"_s, u"Jane Doe"_s, now, now, now, {}, 0, {}, {});
+    User u3(
+        1, User::Registered, u"user@example.net"_s, u"John Doe"_s, now, now, now, {}, 0, {}, {});
+    User u4;
+    User u5(0, User::Invalid, {}, {}, {}, {}, {}, {}, 0, {}, {});
+    User u6 = u1;
+
+    QVERIFY(u1 != u2);
+    QVERIFY(u1 == u3);
+    QVERIFY(u1 != u4);
+    QVERIFY(u1 != u5);
+    QVERIFY(u1 == u6);
+
+    QVERIFY(u4 == u5);
+}
+
+void UserTest::testDatastream()
+{
+    const QDateTime now      = QDateTime::currentDateTimeUtc();
+    const QDateTime created  = now.addDays(-1);
+    const QDateTime updated  = now.addSecs(-5000);
+    const QDateTime lastSeen = now.addSecs(-60);
+    const QVariantMap settings =
+        QVariantMap({{u"timezone"_s, u"Europe/Berlin"_s}, {u"locale"_s, u"de_DE"_s}});
+
+    // test valid into null
+    {
+        User u1(1,
+                User::Manager,
+                u"user@example.net"_s,
+                u"John Doe"_s,
+                created,
+                updated,
+                lastSeen,
+                now,
+                2,
+                u"Jane Doe"_s,
+                settings);
+
+        QByteArray outBa;
+        QDataStream out(&outBa, QIODeviceBase::WriteOnly);
+        out << u1;
+
+        const QByteArray inBa = outBa;
+        QDataStream in(inBa);
+        User u2;
+        in >> u2;
+
+        QCOMPARE(u1, u2);
+    }
+
+    // test valid into valid
+    {
+        User u1(1,
+                User::Manager,
+                u"user@example.net"_s,
+                u"John Doe"_s,
+                created,
+                updated,
+                lastSeen,
+                now,
+                2,
+                u"Jane Doe"_s,
+                settings);
+        User u2(2,
+                User::Administrator,
+                u"user@example.com"_s,
+                u"Jane Doe"_s,
+                created.addSecs(1),
+                updated.addMSecs(2),
+                lastSeen.addSecs(1),
+                now.addSecs(3),
+                1,
+                u"John Doe"_s,
+                {});
+
+        QByteArray outBa;
+        QDataStream out(&outBa, QIODeviceBase::WriteOnly);
+        out << u1;
+
+        const QByteArray inBa = outBa;
+        QDataStream in(inBa);
+        in >> u2;
+
+        QCOMPARE(u1, u2);
+    }
+
+    // test null into valid
+    {
+        User u1;
+        User u2(2,
+                User::Administrator,
+                u"user@example.com"_s,
+                u"Jane Doe"_s,
+                created.addSecs(1),
+                updated.addMSecs(2),
+                lastSeen.addSecs(1),
+                now.addSecs(3),
+                1,
+                u"John Doe"_s,
+                {});
+
+        QByteArray outBa;
+        QDataStream out(&outBa, QIODeviceBase::WriteOnly);
+        out << u1;
+
+        const QByteArray inBa = outBa;
+        QDataStream in(inBa);
+        in >> u2;
+
+        QVERIFY(u2.isNull());
+    }
+
+    // test null into null
+    {
+        User u1, u2;
+
+        QByteArray outBa;
+        QDataStream out(&outBa, QIODeviceBase::WriteOnly);
+        out << u1;
+
+        const QByteArray inBa = outBa;
+        QDataStream in(inBa);
+        in >> u2;
+
+        QVERIFY(u2.isNull());
+    }
+}
+
+void UserTest::testToJson()
+{
+    const QDateTime now      = QDateTime::currentDateTimeUtc();
+    const QDateTime created  = now.addDays(-1);
+    const QDateTime updated  = now.addSecs(-5000);
+    const QDateTime lastSeen = now.addSecs(-60);
+    const QVariantMap settings =
+        QVariantMap({{u"timezone"_s, u"Europe/Berlin"_s}, {u"locale"_s, u"de_DE"_s}});
+
+    User u1(1,
+            User::Manager,
+            u"user@example.net"_s,
+            u"John Doe"_s,
+            created,
+            updated,
+            lastSeen,
+            now,
+            2,
+            u"Jane Doe"_s,
+            settings);
+
+    QJsonObject json = u1.toJson();
+    QCOMPARE(json.value("id"_L1), QJsonValue(1));
+    QCOMPARE(json.value("type"_L1), QJsonValue(static_cast<int>(User::Manager)));
+    QCOMPARE(json.value("email"_L1), QJsonValue(u"user@example.net"_s));
+    QCOMPARE(json.value("displayName"_L1), QJsonValue(u"John Doe"_s));
+    QCOMPARE(json.value("created"_L1), QJsonValue(created.toString(Qt::ISODateWithMs)));
+    QCOMPARE(json.value("updated"_L1), QJsonValue(updated.toString(Qt::ISODateWithMs)));
+    QCOMPARE(json.value("lastSeen"_L1), QJsonValue(lastSeen.toString(Qt::ISODateWithMs)));
+    QCOMPARE(json.value("lockedAt"_L1), QJsonValue(now.toString(Qt::ISODateWithMs)));
+    QCOMPARE(json.value("lockedById"_L1), QJsonValue(2));
+    QCOMPARE(json.value("lockedByName"_L1), QJsonValue(u"Jane Doe"_s));
+    QCOMPARE(json.value("settings"_L1), QJsonValue(QJsonObject::fromVariantMap(settings)));
+
+    User u2(2,
+            User::Administrator,
+            u"user@example.com"_s,
+            u"Jane Doe"_s,
+            created,
+            updated,
+            {},
+            {},
+            0,
+            {},
+            {});
+    json = u2.toJson();
+    QCOMPARE(json.value("lastSeen"_L1), QJsonValue());
+    QCOMPARE(json.value("lockedAt"_L1), QJsonValue());
+    QCOMPARE(json.value("lockedByName"_L1), QJsonValue());
+
+    User u3;
+    json = u3.toJson();
+    QVERIFY(json.isEmpty());
+}
+
+void UserTest::testTypeStringToEnum()
+{
+    QCOMPARE(User::typeStringToEnum(u"invalid"_s), User::Invalid);
+    QCOMPARE(User::typeStringToEnum(u"KatZenSCHEIsse"_s), User::Invalid);
+    QCOMPARE(User::typeStringToEnum(u"DiSabled"_s), User::Disabled);
+    QCOMPARE(User::typeStringToEnum(u"reGIstered"_s), User::Registered);
+    QCOMPARE(User::typeStringToEnum(u"Manager"_s), User::Manager);
+    QCOMPARE(User::typeStringToEnum(u"AdMiNiStRaToR"_s), User::Administrator);
+    QCOMPARE(User::typeStringToEnum(u"SuperUser"_s), User::SuperUser);
+}
+
+void UserTest::testTypeEnumToString()
+{
+    QCOMPARE(User::typeEnumToString(User::Manager), u"Manager"_s);
+}
+
+void UserTest::testSupportedTypes()
+{
+    const QStringList lst = User::supportedTypes();
+    QVERIFY(!lst.empty());
+    QVERIFY(lst.contains(u"Administrator"));
+    QVERIFY(!lst.contains(u"Invalid"));
+}
+
+void UserTest::testToDbId()
+{
+    bool ok         = false;
+    User::dbid_t id = 0;
+
+    id = User::toDbId(123, &ok);
+    QVERIFY(ok);
+    QCOMPARE(id, 123);
+
+    id = User::toDbId(-1, &ok);
+    QVERIFY(!ok);
+    QCOMPARE(id, 0);
+
+    id = User::toDbId(static_cast<qulonglong>(std::numeric_limits<User::dbid_t>::max()) + 4, &ok);
+    QVERIFY(!ok);
+    QCOMPARE(id, 0);
+
+    id = User::toDbId(QVariant::fromValue(123), &ok);
+    QVERIFY(ok);
+    QCOMPARE(id, 123);
+
+    id = User::toDbId(QVariant::fromValue(-1), &ok);
+    QVERIFY(!ok);
+    QCOMPARE(id, 0);
+
+    id = User::toDbId(
+        QVariant::fromValue(static_cast<qulonglong>(std::numeric_limits<User::dbid_t>::max()) + 4),
+        &ok);
+    QVERIFY(!ok);
+    QCOMPARE(id, 0);
+
+    id = User::toDbId(u"123"_s, &ok);
+    QVERIFY(ok);
+    QCOMPARE(id, 123);
+
+    id = User::toDbId(u"-1"_s, &ok);
+    QVERIFY(!ok);
+    QCOMPARE(id, 0);
+
+    id = User::toDbId(u"4294967299"_s, &ok);
+    QVERIFY(!ok);
+    QCOMPARE(id, 0);
 }
 // NOLINTEND(cppcoreguidelines-avoid-do-while)
 
