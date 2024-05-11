c49207d43f10afda2b3d877140457e15ee6d622d
Buschmann <mf@huessenbergnetz.de>
2024-05-11 19:19:56 +0200

    Add template metadata file

--- app/settings.cpp
+++ app/settings.cpp
@@ -10,7 +10,12 @@
 
 #include <Cutelyst/Context>
 
+#include <QFile>
+#include <QFileInfo>
 #include <QGlobalStatic>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonParseError>
 #include <QReadLocker>
 #include <QReadWriteLock>
 #include <QTimeZone>
@@ -31,7 +36,10 @@ struct SettingVals {
     QVector<QLocale> supportedLocales;
 
     QString setupToken;
-    QString tmpl;
+    QString tmpl    = QStringLiteral(HBNBOTA_CONF_CORE_TEMPLATE_DEFVAL);
+    QString tmplDir = QStringLiteral(HBNBOTA_TEMPLATESDIR);
+
+    QHash<QString, QString> tmplIcons;
 
     QLocale defLocale{QStringLiteral(HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL)};
     QTimeZone defTimeZone{QByteArrayLiteral(HBNBOTA_CONF_DEFAULTS_TZ_DEFVAL)};
@@ -49,13 +57,55 @@ Q_GLOBAL_STATIC(SettingVals, cfg) // NOLINT(cppcoreguidelines-avoid-non-const-gl
 bool loadCore(const QVariantMap &core)
 {
     cfg->setupToken = core.value(QStringLiteral(HBNBOTA_CONF_CORE_SETUPTOKEN)).toString();
-    cfg->tmpl       = core.value(QStringLiteral(HBNBOTA_CONF_CORE_TEMPLATE),
-                           QStringLiteral(HBNBOTA_CONF_CORE_TEMPLATE_DEFVAL))
-                    .toString();
 
-    const QString _statPlugin = core.value(QStringLiteral(HBNBOTA_CONF_CORE_STATICPLUGIN),
-                                           QStringLiteral(HBNBOTA_CONF_CORE_STATICPLUGIN_DEFVAL))
-                                    .toString();
+    cfg->tmpl =
+        core.value(QStringLiteral(HBNBOTA_CONF_CORE_TEMPLATE), QStringLiteral(HBNBOTA_CONF_CORE_TEMPLATE_DEFVAL)).toString();
+    if (cfg->tmpl.startsWith('/'_L1)) {
+        auto fullPathParts = cfg->tmpl.split('/'_L1, Qt::SkipEmptyParts);
+        cfg->tmpl          = fullPathParts.takeLast();
+        cfg->tmplDir       = '/'_L1 + fullPathParts.join('/'_L1);
+    }
+
+    const QFileInfo tmplDirFi{cfg->tmplDir + '/'_L1 + cfg->tmpl};
+    if (Q_UNLIKELY(!tmplDirFi.exists())) {
+        qCCritical(HBNBOTA_SETTINGS) << "Template directory" << tmplDirFi.absoluteFilePath() << "does not exist";
+        return false;
+    }
+
+    if (Q_UNLIKELY(!tmplDirFi.isDir())) {
+        qCCritical(HBNBOTA_SETTINGS) << "Template directory path" << tmplDirFi.absoluteFilePath()
+                                     << "dows not point to a directory";
+        return false;
+    }
+
+    QFile tmplMetaDataFile(cfg->tmplDir + '/'_L1 + cfg->tmpl + "/metadata.json"_L1);
+    if (Q_UNLIKELY(!tmplMetaDataFile.exists())) {
+        qCCritical(HBNBOTA_SETTINGS) << "Can not find template meta data file at" << tmplMetaDataFile.fileName();
+        return false;
+    }
+
+    if (Q_UNLIKELY(!tmplMetaDataFile.open(QIODeviceBase::ReadOnly | QIODeviceBase::Text))) {
+        qCCritical(HBNBOTA_SETTINGS) << "Failed to open template meta data file:" << tmplMetaDataFile.errorString();
+        return false;
+    }
+
+    QJsonParseError jpe;
+    const QJsonDocument json = QJsonDocument::fromJson(tmplMetaDataFile.readAll(), &jpe);
+
+    if (Q_UNLIKELY(jpe.error != QJsonParseError::NoError)) {
+        qCCritical(HBNBOTA_SETTINGS) << "Failed to parse template meta data JSON file:" << jpe.errorString();
+        return false;
+    }
+
+    const QJsonObject mdo = json.object();
+    if (Q_UNLIKELY(mdo.isEmpty())) {
+        qCCritical(HBNBOTA_SETTINGS) << "Template meta data file is empty";
+        return false;
+    }
+
+    const QString _statPlugin =
+        core.value(QStringLiteral(HBNBOTA_CONF_CORE_STATICPLUGIN), QStringLiteral(HBNBOTA_CONF_CORE_STATICPLUGIN_DEFVAL))
+            .toString();
     if (_statPlugin.compare("none"_L1, Qt::CaseInsensitive) == 0) {
         cfg->staticPlugin = Settings::StaticPlugin::None;
     } else if (_statPlugin.compare("simple"_L1, Qt::CaseInsensitive) == 0) {
@@ -63,36 +113,32 @@ bool loadCore(const QVariantMap &core)
     } else if (_statPlugin.compare("compressed"_L1, Qt::CaseInsensitive) == 0) {
         cfg->staticPlugin = Settings::StaticPlugin::Compressed;
     } else {
-        qCWarning(HBNBOTA_SETTINGS)
-            << "Invalid value for" << HBNBOTA_CONF_CORE_STATICPLUGIN << "in section"
-            << HBNBOTA_CONF_CORE
-            << ", using default value:" << HBNBOTA_CONF_CORE_STATICPLUGIN_DEFVAL;
+        qCWarning(HBNBOTA_SETTINGS) << "Invalid value for" << HBNBOTA_CONF_CORE_STATICPLUGIN << "in section"
+                                    << HBNBOTA_CONF_CORE
+                                    << ", using default value:" << HBNBOTA_CONF_CORE_STATICPLUGIN_DEFVAL;
     }
 
-    const QString _cache = core.value(QStringLiteral(HBNBOTA_CONF_CORE_CACHE),
-                                      QStringLiteral(HBNBOTA_CONF_CORE_CACHE_DEFVAL))
-                               .toString();
+    const QString _cache =
+        core.value(QStringLiteral(HBNBOTA_CONF_CORE_CACHE), QStringLiteral(HBNBOTA_CONF_CORE_CACHE_DEFVAL)).toString();
     if (_cache.compare("none"_L1, Qt::CaseInsensitive) == 0) {
         cfg->cache = Settings::Cache::None;
     } else if (_cache.compare("memcached"_L1, Qt::CaseInsensitive) == 0) {
         cfg->cache = Settings::Cache::Memcached;
     } else {
-        qCWarning(HBNBOTA_SETTINGS)
-            << "Invalid value for" << HBNBOTA_CONF_CORE_CACHE << "in section" << HBNBOTA_CONF_CORE
-            << ", using, default value:" << HBNBOTA_CONF_CORE_CACHE_DEFVAL;
+        qCWarning(HBNBOTA_SETTINGS) << "Invalid value for" << HBNBOTA_CONF_CORE_CACHE << "in section" << HBNBOTA_CONF_CORE
+                                    << ", using, default value:" << HBNBOTA_CONF_CORE_CACHE_DEFVAL;
     }
 
-    const QString _sessionStore = core.value(QStringLiteral(HBNBOTA_CONF_CORE_SESSIONSTORE),
-                                             QStringLiteral(HBNBOTA_CONF_CORE_SESSIONSTORE_DEFVAL))
-                                      .toString();
+    const QString _sessionStore =
+        core.value(QStringLiteral(HBNBOTA_CONF_CORE_SESSIONSTORE), QStringLiteral(HBNBOTA_CONF_CORE_SESSIONSTORE_DEFVAL))
+            .toString();
     if (_sessionStore.compare("file"_L1, Qt::CaseInsensitive) == 0) {
         cfg->sessionStore = Settings::SessionStore::File;
     } else if (_sessionStore.compare("memcached"_L1, Qt::CaseInsensitive) == 0) {
         cfg->sessionStore = Settings::SessionStore::Memcached;
     } else {
-        qCWarning(HBNBOTA_SETTINGS)
-            << "Invalid value for" << HBNBOTA_CONF_CORE_SESSIONSTORE << "in section"
-            << HBNBOTA_CONF_CORE << ", using default:" << HBNBOTA_CONF_CORE_SESSIONSTORE_DEFVAL;
+        qCWarning(HBNBOTA_SETTINGS) << "Invalid value for" << HBNBOTA_CONF_CORE_SESSIONSTORE << "in section"
+                                    << HBNBOTA_CONF_CORE << ", using default:" << HBNBOTA_CONF_CORE_SESSIONSTORE_DEFVAL;
     }
 
     return true;
@@ -101,33 +147,28 @@ bool loadCore(const QVariantMap &core)
 bool loadDefaults(const QVariantMap &defaults)
 {
     if (!cfg->localesLoaded) {
-        qCCritical(HBNBOTA_SETTINGS)
-            << "Can not load defaults while supported languages are not loaded";
+        qCCritical(HBNBOTA_SETTINGS) << "Can not load defaults while supported languages are not loaded";
         return false;
     }
 
-    const QTimeZone tz{defaults
-                           .value(QStringLiteral(HBNBOTA_CONF_DEFAULTS_TZ),
-                                  QStringLiteral(HBNBOTA_CONF_DEFAULTS_TZ_DEFVAL))
-                           .toString()
-                           .toLatin1()};
+    const QTimeZone tz{
+        defaults.value(QStringLiteral(HBNBOTA_CONF_DEFAULTS_TZ), QStringLiteral(HBNBOTA_CONF_DEFAULTS_TZ_DEFVAL))
+            .toString()
+            .toLatin1()};
     if (tz.isValid()) {
         cfg->defTimeZone = tz;
     } else {
-        qCWarning(HBNBOTA_SETTINGS)
-            << "Invalid value for" << HBNBOTA_CONF_DEFAULTS_TZ << "in section"
-            << HBNBOTA_CONF_DEFAULTS << ", using default value:" << HBNBOTA_CONF_DEFAULTS_TZ_DEFVAL;
+        qCWarning(HBNBOTA_SETTINGS) << "Invalid value for" << HBNBOTA_CONF_DEFAULTS_TZ << "in section"
+                                    << HBNBOTA_CONF_DEFAULTS << ", using default value:" << HBNBOTA_CONF_DEFAULTS_TZ_DEFVAL;
     }
 
-    QLocale defLocale{defaults
-                          .value(QStringLiteral(HBNBOTA_CONF_DEFAULTS_LANG),
-                                 QStringLiteral(HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL))
-                          .toString()};
+    QLocale defLocale{
+        defaults.value(QStringLiteral(HBNBOTA_CONF_DEFAULTS_LANG), QStringLiteral(HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL))
+            .toString()};
     if (defLocale == QLocale::c()) {
-        qCWarning(HBNBOTA_SETTINGS)
-            << "Invalid value for" << HBNBOTA_CONF_DEFAULTS_LANG << "in section"
-            << HBNBOTA_CONF_DEFAULTS
-            << ", using default value:" << HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL;
+        qCWarning(HBNBOTA_SETTINGS) << "Invalid value for" << HBNBOTA_CONF_DEFAULTS_LANG << "in section"
+                                    << HBNBOTA_CONF_DEFAULTS
+                                    << ", using default value:" << HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL;
         defLocale = QLocale{QStringLiteral(HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL)};
     }
 
@@ -141,10 +182,9 @@ bool loadDefaults(const QVariantMap &defaults)
     if (foundLocale) {
         cfg->defLocale = defLocale;
     } else {
-        qCWarning(HBNBOTA_SETTINGS)
-            << defLocale << "set to" << HBNBOTA_CONF_DEFAULTS_LANG << "in section"
-            << HBNBOTA_CONF_DEFAULTS
-            << "is not supported, using default value:" << HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL;
+        qCWarning(HBNBOTA_SETTINGS) << defLocale << "set to" << HBNBOTA_CONF_DEFAULTS_LANG << "in section"
+                                    << HBNBOTA_CONF_DEFAULTS
+                                    << "is not supported, using default value:" << HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL;
         cfg->defLocale = QLocale{QStringLiteral(HBNBOTA_CONF_DEFAULTS_LANG_DEFVAL)};
     }
 
@@ -203,13 +243,13 @@ QString Settings::tmpl()
 QString Settings::tmplPath()
 {
     QReadLocker locker(&cfg->lock);
-    return QStringLiteral(HBNBOTA_TEMPLATESDIR) + '/'_L1 + cfg->tmpl;
+    return cfg->tmplDir + '/'_L1 + cfg->tmpl;
 }
 
 QString Settings::tmplPath(QStringView path)
 {
     QReadLocker locker(&cfg->lock);
-    return QStringLiteral(HBNBOTA_TEMPLATESDIR) + '/'_L1 + cfg->tmpl + '/'_L1 + path;
+    return cfg->tmplDir + '/'_L1 + cfg->tmpl + '/'_L1 + path;
 }
 
 QString Settings::siteName()
@@ -247,8 +287,7 @@ QTimeZone Settings::defTimeZone()
     return cfg->defTimeZone;
 }
 
-QList<CutelystForms::Option *>
-    Settings::supportedLocales(Cutelyst::Context *c, const QLocale &selected, QObject *parent)
+QList<CutelystForms::Option *> Settings::supportedLocales(Cutelyst::Context *c, const QLocale &selected, QObject *parent)
 {
     QList<CutelystForms::Option *> lst;
     QReadLocker locker(&cfg->lock);
@@ -259,8 +298,7 @@ QList<CutelystForms::Option *>
             //: %2 will be replaced by the native territory name, and %3 will be replaced
             //: by the locale’s short name in the form language_territory
             //% "%1 (%2) (%3)"
-            c->qtTrId("hbnbota_locale_option_text")
-                .arg(l.nativeLanguageName(), l.nativeTerritoryName(), l.name()),
+            c->qtTrId("hbnbota_locale_option_text").arg(l.nativeLanguageName(), l.nativeTerritoryName(), l.name()),
             l.name(),
             l == selected,
             parent);
@@ -280,8 +318,7 @@ QStringList Settings::allowedLocaleIds()
     return lst;
 }
 
-QList<CutelystForms::Option *> Settings::supportedTimeZones(const QByteArray &selected,
-                                                            QObject *parent)
+QList<CutelystForms::Option *> Settings::supportedTimeZones(const QByteArray &selected, QObject *parent)
 {
     QList<CutelystForms::Option *> lst;
     const QList<QByteArray> tzIds = QTimeZone::availableTimeZoneIds();
