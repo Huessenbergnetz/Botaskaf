86d06a2802c3b19a3e6dba708f5e8b877269ba4c
Buschmann <mf@huessenbergnetz.de>
2024-05-27 18:38:00 +0200

    Add new Recipient class

---
+++ app/objects/recipient.cpp
@@ -0,0 +1,258 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2024 Matthias Fehring <https://www.huessenbergnetz.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "recipient.h"
+
+Recipient::Data::Data(Recipient::dbid_t _id,
+                      Form _form,
+                      QString _fromName,
+                      QString _fromEmail,
+                      QString _toName,
+                      QString _toEmail,
+                      QString _subject,
+                      QString _text,
+                      QString _html,
+                      QVariantMap _settings,
+                      QDateTime _created,
+                      QDateTime _updated,
+                      QDateTime _lockedAt,
+                      User _lockedBy)
+    : QSharedData()
+    , form{std::move(_form)}
+    , lockedBy{std::move(_lockedBy)}
+    , fromName{std::move(_fromName)}
+    , fromEmail{std::move(_fromEmail)}
+    , toName{std::move(_toName)}
+    , toEmail{std::move(_toEmail)}
+    , subject{std::move(_subject)}
+    , text{std::move(_text)}
+    , html{std::move(_html)}
+    , settings{std::move(_settings)}
+    , created{std::move(_created)}
+    , updated{std::move(_updated)}
+    , lockedAt{std::move(_lockedAt)}
+    , id{_id}
+{
+    created.setTimeSpec(Qt::UTC);
+    if (updated.isValid()) {
+        updated.setTimeSpec(Qt::UTC);
+    }
+    if (lockedAt.isValid()) {
+        lockedAt.setTimeSpec(Qt::UTC);
+    }
+}
+
+Recipient::Recipient(Recipient::dbid_t id,
+                     const Form &form,
+                     const QString &fromName,
+                     const QString &fromEmail,
+                     const QString &toName,
+                     const QString &toEmail,
+                     const QString &subject,
+                     const QString &text,
+                     const QString &html,
+                     const QVariantMap &settings,
+                     const QDateTime &created,
+                     const QDateTime &updated,
+                     const QDateTime &lockedAt,
+                     const User &lockedBy)
+    : data{new Recipient::Data{id,
+                               form,
+                               fromName,
+                               fromEmail,
+                               toName,
+                               toEmail,
+                               subject,
+                               text,
+                               html,
+                               settings,
+                               created,
+                               updated,
+                               lockedAt,
+                               lockedBy}}
+{
+}
+
+Recipient::dbid_t Recipient::id() const noexcept
+{
+    return data ? data->id : 0;
+}
+
+Form Recipient::form() const noexcept
+{
+    return data ? data->form : Form();
+}
+
+QString Recipient::fromName() const noexcept
+{
+    return data ? data->fromName : QString();
+}
+
+QString Recipient::fromEmail() const noexcept
+{
+    return data ? data->fromEmail : QString();
+}
+
+QString Recipient::toName() const noexcept
+{
+    return data ? data->toName : QString();
+}
+
+QString Recipient::toEmail() const noexcept
+{
+    return data ? data->toEmail : QString();
+}
+
+QString Recipient::subject() const noexcept
+{
+    return data ? data->subject : QString();
+}
+
+QString Recipient::text() const noexcept
+{
+    return data ? data->text : QString();
+}
+
+QString Recipient::html() const noexcept
+{
+    return data ? data->html : QString();
+}
+
+QVariantMap Recipient::settings() const noexcept
+{
+    return data ? data->settings : QVariantMap();
+}
+
+QDateTime Recipient::created() const noexcept
+{
+    return data ? data->created : QDateTime();
+}
+
+QDateTime Recipient::updated() const noexcept
+{
+    return data ? data->updated : QDateTime();
+}
+
+QDateTime Recipient::lockedAt() const noexcept
+{
+    return data ? data->lockedAt : QDateTime();
+}
+
+User Recipient::lockedBy() const noexcept
+{
+    return data ? data->lockedBy : User();
+}
+
+bool Recipient::isValid() const noexcept
+{
+    return data && data->id > 0;
+}
+
+Recipient::dbid_t Recipient::toDbId(qulonglong id, bool *ok)
+{
+    if (id > static_cast<qulonglong>(std::numeric_limits<Recipient::dbid_t>::max())) {
+        if (ok) {
+            *ok = false;
+        }
+        return 0;
+    }
+
+    if (ok) {
+        *ok = true;
+    }
+
+    return static_cast<Recipient::dbid_t>(id);
+}
+
+Recipient::dbid_t Recipient::toDbId(const QVariant &var, bool *ok)
+{
+    bool _ok      = false;
+    const auto id = var.toULongLong(&_ok);
+    if (_ok) {
+        return toDbId(id, ok);
+    }
+
+    if (ok) {
+        *ok = false;
+    }
+
+    return 0;
+}
+
+QDebug operator<<(QDebug dbg, const Recipient &recipient)
+{
+    QDebugStateSaver saver(dbg);
+    dbg.nospace() << "Recipient(";
+    if (!recipient.isNull()) {
+        if (recipient.isValid()) {
+            dbg << "ID: " << recipient.id();
+            dbg << ", Form ID: " << recipient.form().id();
+            dbg << ", From: ";
+            if (recipient.fromName().isEmpty()) {
+                dbg << recipient.fromEmail();
+            } else {
+                dbg << recipient.fromName() << " <" << recipient.fromEmail() << '>';
+            }
+            dbg << ", To: ";
+            if (recipient.toName().isEmpty()) {
+                dbg << recipient.toEmail();
+            } else {
+                dbg << recipient.toName() << " <" << recipient.toEmail() << '>';
+            }
+            dbg << ", Subject: " << recipient.subject();
+        } else {
+            dbg << "INVALID";
+        }
+    }
+    dbg << ')';
+    return dbg;
+}
+
+QDataStream &operator<<(QDataStream &out, const Recipient &recipient)
+{
+    if (!recipient.isNull()) {
+        out << recipient.data->id << recipient.data->form << recipient.data->fromName << recipient.data->fromEmail
+            << recipient.data->toName << recipient.data->toEmail << recipient.data->subject << recipient.data->text
+            << recipient.data->html << recipient.data->settings << recipient.data->created << recipient.data->updated
+            << recipient.data->lockedAt << recipient.data->lockedBy;
+    } else {
+        out << static_cast<Recipient::dbid_t>(0);
+    }
+
+    return out;
+}
+
+QDataStream &operator>>(QDataStream &in, Recipient &recipient)
+{
+    Recipient::dbid_t id = 0;
+    in >> id;
+
+    if (id == 0) {
+        recipient.clear();
+    } else {
+        if (recipient.data == nullptr) {
+            recipient.data = new Recipient::Data;
+        }
+
+        recipient.data->id = id;
+        in >> recipient.data->form;
+        in >> recipient.data->fromName;
+        in >> recipient.data->fromEmail;
+        in >> recipient.data->toName;
+        in >> recipient.data->toEmail;
+        in >> recipient.data->subject;
+        in >> recipient.data->text;
+        in >> recipient.data->html;
+        in >> recipient.data->settings;
+        in >> recipient.data->created;
+        in >> recipient.data->updated;
+        in >> recipient.data->lockedAt;
+        in >> recipient.data->lockedBy;
+    }
+
+    return in;
+}
+
+#include "moc_recipient.cpp"
---
+++ app/objects/recipient.h
@@ -0,0 +1,190 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2024 Matthias Fehring <https://www.huessenbergnetz.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef HBNBOTA_RECIPIENT_H
+#define HBNBOTA_RECIPIENT_H
+
+#include "form.h"
+#include "user.h"
+
+#include <QDateTime>
+#include <QObject>
+#include <QSharedDataPointer>
+
+/*!
+ * \brief Contains data about a contact form recipient.
+ */
+class Recipient
+{
+    Q_GADGET
+    Q_PROPERTY(Recipient::dbid_t id READ id CONSTANT)
+    Q_PROPERTY(Form form READ form CONSTANT)
+    Q_PROPERTY(QString fromName READ fromName CONSTANT)
+    Q_PROPERTY(QString fromEmail READ fromEmail CONSTANT)
+    Q_PROPERTY(QString toName READ toName CONSTANT)
+    Q_PROPERTY(QString toEmail READ toEmail CONSTANT)
+    Q_PROPERTY(QString subject READ subject CONSTANT)
+    Q_PROPERTY(QString text READ text CONSTANT)
+    Q_PROPERTY(QString html READ html CONSTANT)
+    Q_PROPERTY(QVariantMap settings READ settings CONSTANT)
+    Q_PROPERTY(QDateTime created READ created CONSTANT)
+    Q_PROPERTY(QDateTime updated READ updated CONSTANT)
+    Q_PROPERTY(QDateTime lockedAt READ lockedAt CONSTANT)
+    Q_PROPERTY(User lockedBy READ lockedBy CONSTANT)
+public:
+    using dbid_t = quint32;
+
+    Recipient() noexcept = default;
+
+    Recipient(Recipient::dbid_t id,
+              const Form &form,
+              const QString &fromName,
+              const QString &fromEmail,
+              const QString &toName,
+              const QString &toEmail,
+              const QString &subject,
+              const QString &text,
+              const QString &html,
+              const QVariantMap &settings,
+              const QDateTime &created,
+              const QDateTime &updated,
+              const QDateTime &lockedAt,
+              const User &lockedBy);
+
+    Recipient(const Recipient &other) noexcept            = default;
+    Recipient(Recipient &&other) noexcept                 = default;
+    Recipient &operator=(const Recipient &other) noexcept = default;
+    Recipient &operator=(Recipient &&other) noexcept      = default;
+    ~Recipient() noexcept                                 = default;
+
+    void swap(Recipient &other) noexcept { data.swap(other.data); }
+
+    [[nodiscard]] dbid_t id() const noexcept;
+
+    [[nodiscard]] Form form() const noexcept;
+
+    [[nodiscard]] QString fromName() const noexcept;
+
+    [[nodiscard]] QString fromEmail() const noexcept;
+
+    [[nodiscard]] QString toName() const noexcept;
+
+    [[nodiscard]] QString toEmail() const noexcept;
+
+    [[nodiscard]] QString subject() const noexcept;
+
+    [[nodiscard]] QString text() const noexcept;
+
+    [[nodiscard]] QString html() const noexcept;
+
+    [[nodiscard]] QVariantMap settings() const noexcept;
+
+    [[nodiscard]] QDateTime created() const noexcept;
+
+    [[nodiscard]] QDateTime updated() const noexcept;
+
+    [[nodiscard]] QDateTime lockedAt() const noexcept;
+
+    [[nodiscard]] User lockedBy() const noexcept;
+
+    [[nodiscard]] bool isValid() const noexcept;
+
+    [[nodiscard]] bool isNull() const noexcept { return !data; }
+
+    void clear()
+    {
+        if (!isNull()) {
+            *this = Recipient();
+        }
+    }
+
+    /*!
+     * \brief Returns \a id casted into dbid_t.
+     *
+     * Returns \c 0 if the value would overflow the dbid_t.
+     *
+     * If \a ok is not \c nullptr, failure is reported by setting \a *ok to \c false,
+     * and success by setting \a *ok to \c true.
+     */
+    static dbid_t toDbId(qulonglong id, bool *ok = nullptr);
+
+    /*!
+     * \brief Returns \a var converted to dbid_t.
+     *
+     * Returns \c 0 if the conversion fails.
+     *
+     * If \a ok is not \c nullptr, failure is reported by setting \a *ok to \c false,
+     * and success by setting \a *ok to \c true.
+     */
+    static dbid_t toDbId(const QVariant &var, bool *ok = nullptr);
+
+private:
+    class Data : public QSharedData // NOLINT(cppcoreguidelines-special-member-functions)
+    {
+    public:
+        Data() noexcept = default;
+        Data(Recipient::dbid_t _id,
+             Form _form,
+             QString _fromName,
+             QString _fromEmail,
+             QString _toName,
+             QString _toEmail,
+             QString _subject,
+             QString _text,
+             QString _html,
+             QVariantMap _settings,
+             QDateTime _created,
+             QDateTime _updated,
+             QDateTime _lockedAt,
+             User _lockedBy);
+
+        Data(const Data &) noexcept   = default;
+        Data &operator=(const Data &) = delete;
+        ~Data() noexcept              = default;
+
+        Form form;
+        User lockedBy;
+        QString fromName;
+        QString fromEmail;
+        QString toName;
+        QString toEmail;
+        QString subject;
+        QString text;
+        QString html;
+        QVariantMap settings;
+        QDateTime created;
+        QDateTime updated;
+        QDateTime lockedAt;
+        Recipient::dbid_t id{0};
+    };
+
+    QSharedDataPointer<Data> data;
+
+    friend QDataStream &operator<<(QDataStream &out, const Recipient &recipient);
+    friend QDataStream &operator>>(QDataStream &in, Recipient &recipient);
+};
+
+Q_DECLARE_SHARED(Recipient) // NOLINT(modernize-type-traits)
+Q_DECLARE_METATYPE(Recipient)
+
+/*!
+ * \related Form
+ * \brief Writes the \a recipient to the debug stream \a dbg and returns a refererence to the stream.
+ */
+QDebug operator<<(QDebug dbg, const Recipient &recipient);
+
+/*!
+ * \related Form
+ * \brief Writes \a recipient to stream \a out. Returns a reference to the stream.
+ */
+QDataStream &operator<<(QDataStream &out, const Recipient &recipient);
+
+/*!
+ * \related Form
+ * \brief Reads a recipient from stream \a in into \a recipient. Returns a reference to the stream.
+ */
+QDataStream &operator>>(QDataStream &in, Recipient &recipient);
+
+#endif // HBNBOTA_RECIPIENT_H
